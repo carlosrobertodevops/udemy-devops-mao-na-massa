18b82dbfcb1d5fa2c43826a4c2dacfb7a2b1ce2e
Carlos Roberto dos Santos Silva <tenroberto2011@gmail.com>
2022-08-30 08:40:05 -0300

    Ajustes

--- ansible-lab/lab-destroy.sh
+++ ansible-lab/lab-destroy.sh
@@ -1,15 +1,15 @@
 #/bin/zsh
 echo 'SUSPENDENDO: control-none ...'
 cd control-node
-vagrant suspend
+vagrant destroy
 cd ..
 
 echo 'SUSPENDENDO: app01'
 cd app01
-vagrant suspend
+vagrant destroy
 cd ..
 
 echo 'SUSPENDENDO: db01'
 cd db01
-vagrant suspend
+vagrant destroy
 cd ..
--- ansible-lab/lab-init.sh
+++ ansible-lab/lab-init.sh
@@ -1,15 +1,15 @@
 #/bin/zsh
 echo '1. INICIANDO => control-none ...'
 cd control-node
-vagrant up --provision
+vagrant init
 cd ..
 
 echo '2. INICIANDO => app01'
 cd app01
-vagrant up --provision
+vagrant init
 cd ..
 
 echo '3. INICIANDO => db01'
 cd db01
-vagrant up --provision
+vagrant init
 cd ..
